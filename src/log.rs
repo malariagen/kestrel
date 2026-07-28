3cdc7ee69bd92daaae91abddda4eade2783c446b
Jacob Garber <jacob@jwgarber.ca>
2026-07-28 12:19:17 -0600

    add log

---
+++ src/log.rs
@@ -0,0 +1,130 @@
+use core::arch::x86_64::*;
+
+// Adapted from https://github.com/burrbull/sleef-rs/blob/master/src/f64x/u10.rs
+// and https://github.com/shibatch/sleef/blob/master/src/libm/sleefsimddp.c
+
+#[inline]
+#[target_feature(enable = "avx512f")]
+// This uses 11 registers total (from the assembly)
+pub fn log_avx512(d: __m512d) -> __m512d {
+    let p = _mm512_mul_pd(d, _mm512_set1_pd(1.0 / 0.75));
+    let mut e = _mm512_getexp_pd(p);
+    let mask = _mm512_cmp_pd_mask(e, _mm512_set1_pd(f64::INFINITY), _CMP_EQ_OQ);
+    e = _mm512_mask_blend_pd(mask, e, _mm512_set1_pd(1024.0));
+
+    let m = _mm512_getmant_pd(d, _MM_MANT_NORM_P75_1P5, _MM_MANT_SIGN_NAN);
+
+    let log2_hi = _mm512_set1_pd(0.693_147_180_559_945_286_226_764);
+    let log2_lo = _mm512_set1_pd(2.319_046_813_846_299_558_417_771_e-17);
+    let mut s = mul((log2_hi, log2_lo), e);
+
+    let u = add_as_doubled(_mm512_set1_pd(-1.0), m);
+    let l = add_as_doubled(_mm512_set1_pd(1.0), m);
+    let x = div(u, l);
+    let x2 = _mm512_mul_pd(x.0, x.0);
+    let x4 = _mm512_mul_pd(x2, x2);
+    let x8 = _mm512_mul_pd(x4, x4);
+
+    let t = poly7(
+        x2,
+        x4,
+        x8,
+        0.153_207_698_850_270_135_3,
+        0.152_562_905_100_342_871_6,
+        0.181_860_593_293_778_599_6,
+        0.222_221_451_983_938_000_9,
+        0.285_714_293_279_429_931_7,
+        0.399_999_999_963_525_199,
+        0.666_666_666_666_733_354_1,
+    );
+
+    let two = _mm512_set1_pd(2.0);
+    s = add_checked_double(s, (_mm512_mul_pd(x.0, two), _mm512_mul_pd(x.1, two)));
+    s = add_checked(s, _mm512_mul_pd(_mm512_mul_pd(x2, x.0), t));
+
+    let r = _mm512_add_pd(s.0, s.1);
+
+    let control: i64 = (4 << (2 * 4)) | (3 << (4 * 4)) | (5 << (5 * 4)) | (2 << (6 * 4));
+    _mm512_fixupimm_pd(r, d, _mm512_set1_epi64(control), 0)
+}
+
+#[inline]
+#[target_feature(enable = "avx512f")]
+fn add_as_doubled(s: __m512d, o: __m512d) -> (__m512d, __m512d) {
+    let r0 = _mm512_add_pd(s, o);
+    let v = _mm512_sub_pd(r0, s);
+
+    let a = _mm512_sub_pd(r0, v);
+    let b = _mm512_sub_pd(o, v);
+    let c = _mm512_sub_pd(s, a);
+    let d = _mm512_add_pd(c, b);
+    (r0, d)
+}
+
+#[inline]
+#[target_feature(enable = "avx512f")]
+fn add_checked_double(s: (__m512d, __m512d), o: (__m512d, __m512d)) -> (__m512d, __m512d) {
+    let r0 = _mm512_add_pd(s.0, o.0);
+
+    let a = _mm512_sub_pd(s.0, r0);
+    let b = _mm512_add_pd(a, o.0);
+    let c = _mm512_add_pd(b, s.1);
+    let d = _mm512_add_pd(c, o.1);
+    (r0, d)
+}
+
+#[inline]
+#[target_feature(enable = "avx512f")]
+fn add_checked(s: (__m512d, __m512d), o: __m512d) -> (__m512d, __m512d) {
+    let r0 = _mm512_add_pd(s.0, o);
+
+    let a = _mm512_sub_pd(s.0, r0);
+    let b = _mm512_add_pd(a, o);
+    let c = _mm512_add_pd(b, s.1);
+    (r0, c)
+}
+
+#[inline]
+#[target_feature(enable = "avx512f")]
+fn mul(s: (__m512d, __m512d), o: __m512d) -> (__m512d, __m512d) {
+    let r0 = _mm512_mul_pd(s.0, o);
+    let a = _mm512_fmadd_pd(s.1, o, _mm512_fmsub_pd(s.0, o, r0));
+    (r0, a)
+}
+
+#[inline]
+#[target_feature(enable = "avx512f")]
+fn div(s: (__m512d, __m512d), o: (__m512d, __m512d)) -> (__m512d, __m512d) {
+    let one = _mm512_set1_pd(1.0);
+
+    let t = _mm512_div_pd(one, o.0);
+
+    let q0 = _mm512_mul_pd(s.0, t);
+    let u = _mm512_fmsub_pd(t, s.0, q0);
+
+    let mut q1 = _mm512_fnmadd_pd(o.1, t, _mm512_fnmadd_pd(o.0, t, one));
+    q1 = _mm512_fmadd_pd(q0, q1, _mm512_fmadd_pd(s.1, t, u));
+
+    (q0, q1)
+}
+
+#[inline]
+#[target_feature(enable = "avx512f")]
+fn poly7(x: __m512d, x2: __m512d, x4: __m512d, c6: f64, c5: f64, c4: f64, c3: f64, c2: f64, c1: f64, c0: f64) -> __m512d {
+    _mm512_fmadd_pd(x4, poly3(x, x2, c6, c5, c4), poly4(x, x2, c3, c2, c1, c0))
+}
+
+#[inline]
+#[target_feature(enable = "avx512f")]
+fn poly3(x: __m512d, x2: __m512d, c2: f64, c1: f64, c0: f64) -> __m512d {
+    _mm512_fmadd_pd(x2, _mm512_set1_pd(c2), _mm512_fmadd_pd(x, _mm512_set1_pd(c1), _mm512_set1_pd(c0)))
+}
+
+#[inline]
+#[target_feature(enable = "avx512f")]
+fn poly4(x: __m512d, x2: __m512d, c3: f64, c2: f64, c1: f64, c0: f64) -> __m512d {
+    _mm512_fmadd_pd(x2,
+        _mm512_fmadd_pd(x, _mm512_set1_pd(c3), _mm512_set1_pd(c2)),
+        _mm512_fmadd_pd(x, _mm512_set1_pd(c1), _mm512_set1_pd(c0))
+    )
+}
